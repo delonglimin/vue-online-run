cdabcf3eb2ccac9700e4a7f41c8333f45d4f6f79
xudelong@100tal.com <xudelong@100tal.com>
2019-10-16 15:22:10 +0800

    test jenkins

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
 
-    // Various Dev Server settings
+    // Various Dev Server settings 
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 9000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
