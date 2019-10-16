884248f7602d9662fbffbc33f74442a38ae7ffd4
xudelong@100tal.com <xudelong@100tal.com>
2019-10-16 15:50:20 +0800

    ip

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings 
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: '47.92.73.142', // can be overwritten by process.env.HOST
     port: 9000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
