a16c291e205c4241c104fe3cf6e57b6ed6848628
xudelong@100tal.com <xudelong@100tal.com>
2019-10-16 15:00:27 +0800

    init

---
+++ src/components/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box">
+    <edit v-model="code" @running="running"></edit>
+    <running :value="code" ref="run"></running>
+  </div>
+</template>
+<script>
+import Edit from './edit'
+import Running from './running'
+export default {
+  name: 'index',
+  components: {
+    Edit,
+    Running
+  },
+  data () {
+    return {
+      code: ''
+    }
+  },
+  methods: {
+    running () {
+      this.$refs.run.reset()
+      this.$refs.run.buildDom()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.box {
+  min-width: 1000px;
+  overflow: auto;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
